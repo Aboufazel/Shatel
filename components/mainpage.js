78913aece1f15d8f60146e0e91b787d2089cde69
mohammad sohrabi <mohammadsohrabi141@gmail.com>
2022-08-25 03:36:30 +0430

    add site header new file header.css ,mainpage.html

---
+++ components/mainpage.js
@@ -0,0 +1,20 @@
+var $ = document;
+
+const header__right__Mobile = $.querySelector('.header__right__Mobile');
+const list = $.querySelector('.menu__item__burger');
+
+
+ var bool =true;
+ header__right__Mobile.addEventListener('click', ()=>{
+    if (bool == true) {
+        header__right__Mobile.classList.add('open');
+        list.style.top='50px';
+        bool = false;
+    }
+    else{
+        header__right__Mobile.classList.remove('open');
+        list.style.top='-100%';
+        bool = true;
+    }
+})
+
